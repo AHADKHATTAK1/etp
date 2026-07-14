7ab511eb6c71fabd885008060b824fd9a0d2d34a
ZAIDAN FITNESS <zaidanfitnessgym@gmail.com>
2026-07-15 00:48:07 +0500

    feat: clean URL extensions and introduce js/config.js environment variables configuration

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// ETECHPROVIDER Environment Variables Configuration
+const ENV = {
+  CONTACT_EMAIL: "info@etechprovider.co.uk",
+  PHONE_PRIMARY: "+44 7521 866654",
+  PHONE_SECONDARY: "+44 7599 388347",
+  OFFICE_ADDRESS: "Level 3, 1 Fintech Street, London, EC2A 4NE, United Kingdom"
+};
