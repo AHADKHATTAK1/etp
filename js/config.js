626938bab57312c2312f0294635dd231c3b81505
ZAIDAN FITNESS <zaidanfitnessgym@gmail.com>
2026-07-15 02:36:39 +0500

    style: update office address configuration to Bradford location and customize contact map

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const ENV = {
   CONTACT_EMAIL: "info@etechprovider.co.uk",
   PHONE_PRIMARY: "+44 7521 866654",
   PHONE_SECONDARY: "+44 7599 388347",
-  OFFICE_ADDRESS: "Level 3, 1 Fintech Street, London, EC2A 4NE, United Kingdom"
+  OFFICE_ADDRESS: "275 Legrams Lane, Bradford, BD7 1EE, United Kingdom"
 };
